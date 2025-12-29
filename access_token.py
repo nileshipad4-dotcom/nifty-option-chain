26d9d5961a002fcca2c9bacc0b09bdd30e9568d2
nilesh raj <nileshipad4@gmail.com>
2025-12-29 07:25:52 +0530

    Update access_token.py

--- access_token.py
+++ access_token.py
@@ -1,14 +1,26 @@
 # https://kite.zerodha.com/connect/login?api_key=bkgv59vaazn56c42&v=3
 
+import streamlit as st
 from kiteconnect import KiteConnect
+
+st.title("Zerodha Kite Access Token Generator")
 
 api_key = "bkgv59vaazn56c42"
 api_secret = "sb1sxe6s2p9qbmajwnlfe8bxmxfzbzbf"
-request_token = "9IlK75RVABdWC1BmOmSn3Nyohwnvrct5"
 
-kite = KiteConnect(api_key=api_key)
+request_token = st.text_input("Enter Request Token", type="password")
 
-data = kite.generate_session(request_token, api_secret=api_secret)
-access_token = data["access_token"]
+if st.button("Generate Access Token"):
+    if request_token:
+        try:
+            kite = KiteConnect(api_key=api_key)
+            data = kite.generate_session(request_token, api_secret=api_secret)
+            access_token = data["access_token"]
 
-print("ACCESS TOKEN:", access_token)
+            st.success("Access Token Generated Successfully")
+            st.code(access_token)
+
+        except Exception as e:
+            st.error(f"Error: {e}")
+    else:
+        st.warning("Please enter a request token")
