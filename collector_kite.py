b2c30fcee98b03da49a6040fbceba6c787bd7af7
nilesh raj <nileshipad4@gmail.com>
2026-01-08 07:49:47 +0530

    Update collector_kite.py

--- collector_kite.py
+++ collector_kite.py
@@ -13,7 +13,7 @@ IST = pytz.timezone("Asia/Kolkata")
 # CONFIG
 # ==================================================
 API_KEY = "bkgv59vaazn56c42"
-ACCESS_TOKEN = "QcPcQ6r4mFdL253BIj3H7Vpg5l8JUXti"
+ACCESS_TOKEN = "um1gYW2GgQ94kdg2G1C9vu3cWfdFF00X"
 
 STOCKS = [
     "360ONE","ABB","ABCAPITAL","ADANIENSOL","ADANIENT","ADANIGREEN","ADANIPORTS","ALKEM",
